57970224ece3f4142aa4a3a731791f53b91f3fde
Sticktron <sticktron@hotmail.com>
2017-02-01 11:49:08 -0500

    Leave a copy of Releases after updating

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ echo "--------------------------"
 #./dpkg-scanpackages.pl -m ./debs /dev/null >Packages
 dpkg-scanpackages -m ./debs /dev/null >Packages
 rm -f Packages.bz2
-bzip2 Packages
+bzip2 -k Packages
 
 echo "--------------------------"
 echo "Done."
